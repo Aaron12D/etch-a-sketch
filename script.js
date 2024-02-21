959cf464a42b49f3e0b15f4ffcf311862c5d69f9
Aaron12D <153545330+Aaron12D@users.noreply.github.com>
2024-02-20 21:21:32 -0500

    build etch-a-sketch
    Complete etch-a-sketch with html, css, and javascript.

---
+++ script.js
@@ -0,0 +1,69 @@
+const container = document.querySelector('.container');
+const inputNumber = document.querySelector('#input');
+const div = document.querySelectorAll('div');
+let rgb = ``;
+let inputValue = 16;
+let rainbow = 1;
+
+function createGrid(){
+    for (let i = 1; i <= inputValue; i++){
+            const column = document.createElement('div');
+            column.classList.add('column');
+            container.appendChild(column);
+            for (let i = 1; i<= inputValue; i++){
+            const row = document.createElement('div');
+            row.classList.add('row');
+            column.appendChild(row);
+            };
+    };
+};
+createGrid();
+    
+function removeCurrentGrid(){
+    for (i = 1; i <= inputValue; i++){
+    const rmcolumn = document.querySelector('.column');
+    const rmrow = document.querySelector('.row');
+    rmcolumn.remove();
+    rmrow.remove();
+    };
+};
+
+
+inputNumber.addEventListener('change', function(){
+    removeCurrentGrid(); 
+    inputValue = document.querySelector('#input').value;
+    createGrid();
+});
+
+function randomNumber(){    
+    return Math.floor(Math.random()* 256);
+};
+
+function randomColor(){
+    const colorOne = randomNumber();
+    const colorTwo = randomNumber();
+    const colorThree = randomNumber();
+    rgb = `rgb(${colorOne}, ${colorTwo}, ${colorThree})`;
+}; 
+
+document.addEventListener('mouseover', function(event){
+    if (event.target.classList.value === 'row'){
+        if (rainbow === 1){
+        randomColor();
+        event.target.style.backgroundColor = rgb;
+        } else {
+            event.target.style.backgroundColor = 'white'
+        };
+    };
+});
+
+document.addEventListener('click', function(event){
+    if (event.target.classList.value === 'rainbow'){
+        rainbow = 1;
+    } else if (event.target.classList.value === 'erase') {
+        rainbow = 0;
+    } else if (event.target.classList.value === 'reset'){
+        removeCurrentGrid();
+        createGrid();
+    };
+});
